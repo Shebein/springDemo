aefdf30d3985640c1fa3dbd4dc0f31521ddb016f
Shebein <liqingduo867@163.com>
2024-01-04 16:42:51 +0800

    #提交#

---
+++ src/main/java/org/example/lury/SpringbootDemoApplication.java
@@ -0,0 +1,17 @@
+package org.example.lury;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@MapperScan("com.example.springbootdemo.mapper")
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("AAAA");
+        System.out.println("hello world");
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+}
